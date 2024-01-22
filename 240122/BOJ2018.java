0597ccf4b8f7af6373b51c15d7db34c6e25b638d
Nakyung Lee <68213689+nakyung128@users.noreply.github.com>
2024-01-22 23:09:07 +0900

    add: 240122 연속된 수의 합 문제 풀이

---
+++ 240122/BOJ2018.java
@@ -0,0 +1,33 @@
+package jan_week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2018 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int[] num = new int[n];
+		int answer = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			num[i-1] = i;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			int hap = num[i];
+			for (int j = i+1; j < n; j++) {
+				hap += num[j];
+				if (hap > n) break; // break 안 하면 시간초과 
+				else if (hap == n) answer++;
+				
+			}
+		}
+		
+		System.out.println(answer+1);
+	}
+}
