69f2013988de1f88733d20dbd2d4cc70a41a024f
nakyung128 <lnk12888@naver.com>
2024-10-10 01:54:58 +0900

    BOJ5568_HashSet 사용하여 시간 단축

--- 10_OCT/BOJ5568_카드놓기.java
+++ 10_OCT/BOJ5568_카드놓기.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class BOJ5568_카드놓기 {
     static int n, k;
     static int[] nums;
-    static ArrayList<Integer> per;
-    static ArrayList<String> answers;
+    static ArrayList<Integer> arr;
+    static HashSet<String> hs;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -17,42 +18,33 @@ public class BOJ5568_카드놓기 {
 
         nums = new int[n];
         visited = new boolean[n];
-        per = new ArrayList<>();
-        answers = new ArrayList<>();
+        arr = new ArrayList<>();
+        hs = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(br.readLine());
         }
 
         select(0);
-        System.out.println(answers.size());
+        System.out.println(hs.size());
     }
 
     static void select(int depth) {
         if (depth == k) {
-            String num = "";
-            for (int p : per) {
-                num += String.valueOf(p);
+            StringBuilder sb = new StringBuilder();
+            for (int n : arr) {
+                sb.append(String.valueOf(n));
             }
-            boolean flag = true;
-            for (String s : answers) {
-                if (s.equals(num)) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                answers.add(num);
-            }
+            hs.add(sb.toString());
             return;
         }
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                per.add(nums[i]);
+                arr.add(nums[i]);
                 select(depth + 1);
                 visited[i] = false;
-                per.remove(per.size() - 1);
+                arr.remove(arr.size() - 1);
             }
         }
     }
