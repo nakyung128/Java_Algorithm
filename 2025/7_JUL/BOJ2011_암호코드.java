08e7119c6a5255f68e976728650420a0b29693aa
nakyung128 <lnk12888@naver.com>
2025-07-10 12:35:50 +0900

    fix: 250710 코드 수정

--- 2025/7_JUL/BOJ2011_암호코드.java
+++ 2025/7_JUL/BOJ2011_암호코드.java
@@ -14,7 +14,7 @@ public class BOJ2011_암호코드 {
         } else {
             int[] dp = new int[code.length() + 1];
             dp[0] = 1; // 아무것도 해석 안 했을 때 한 가지
-            dp[1] = 1; // 첫 글자 무조건 한 가지
+            dp[1] = 1;
 
             for (int i = 2; i < code.length() + 1; i++) {
                 char now = code.charAt(i - 1);
@@ -22,13 +22,15 @@ public class BOJ2011_암호코드 {
 
                 // 현재 글자가 1 ~ 9인 경우 혼자도 해석 가능함 (0 아니면)
                 if (now != '0') {
-                    dp[i] = dp[i - 1] % MOD;
+                    dp[i] += dp[i - 1];
+                    dp[i] %= MOD;
                 }
 
                 // 앞 글자랑 현재 글자 합쳐서 10 ~ 26인 경우 두 글자 해석 가능
                 int two = (prev - '0') * 10 + (now - '0');
                 if (two >= 10 && two <= 26) {
-                    dp[i] += dp[i - 2] % MOD;
+                    dp[i] += dp[i - 2];
+                    dp[i] %= MOD;
                 }
             }
             System.out.println(dp[code.length()]);
